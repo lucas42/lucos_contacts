62ceebd309d6382050b7dfa27030f61b94a9344f
Luke Blaney <git@lukeblaney.co.uk>
2015-01-04 02:47:15 +0000

    use custom user class which links directly to agents

--- lucosauth/models.py
+++ lucosauth/models.py
@@ -2,7 +2,8 @@ from django.db import models
 from django import utils
 from local_settings import AUTH_DOMAIN
 from urllib2 import HTTPError
-from django.contrib.auth.models import User
+from django.contrib.auth.models import AbstractBaseUser
+from agents.models import Agent
 import json, time, urllib2, os
 
 class LucosAuthBackend(object):
@@ -15,16 +16,38 @@ class LucosAuthBackend(object):
 		if (data['id'] == None):
 			print "No id returned by auth service; "+url
 			return None
-		username = 'lucos:'+data['id']
 		try:
-			user = User.objects.get(username=username)
-		except User.DoesNotExist:
-			user = User(username=username)
+			agent = Agent.objects.get(id=data['id'])
+		except Agent.DoesNotExist:
+			print "Unknown id returned by auth service; "+url
+			return None
+		try:
+			user = LucosUser.objects.get(agent=agent)
+		except LucosUser.DoesNotExist:
+			user = LucosUser(agent=agent)
 			user.save()
 		return user
 
 	def get_user(self, user_id):
 		try:
-			return User.objects.get(pk=user_id)
-		except User.DoesNotExist:
+			user = LucosUser.objects.get(pk=user_id)
+			return user
+		except LucosUser.DoesNotExist:
 			return None
+
+class LucosUser(AbstractBaseUser):
+	agent = models.OneToOneField(Agent)
+	def is_staff(self):
+		return self.agent.id == 2
+	def has_module_perms(self, app_label):
+		if (app_label == 'agents'):
+			return True
+		return False
+	def has_perm(self, perm, obj=None):
+		if (perm.startswith('agents.')):
+			return True
+		return False
+	def get_short_name(self):
+		return self.agent.getName()
+	def get_long_name(self):
+		return self.agent.getName()
