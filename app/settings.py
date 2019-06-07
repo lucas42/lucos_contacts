43cb9901753ee3d9d96c5f4918e37d1958962c31
Luke Blaney <git@lukeblaney.co.uk>
2019-06-07 22:47:23 +0100

    add postgres database

--- app/settings.py
+++ app/settings.py
@@ -96,6 +96,17 @@ SECURE_PROXY_SSL_HEADER = (
 	'HTTP_X_FORWARDED_PROTO', 'https'
 )
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'postgres',              # Or path to database file if using sqlite3.
+        'USER': 'postgres',              # Not used with sqlite3.
+        'PASSWORD': '',                  # Not used with sqlite3.
+        'HOST': 'db',                    # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+}
+}
+
 '''LOGGING = {
     'version': 1,
     'handlers': {
