1baccd3bce1f3a0379a6f7e004f34042d82eee55
Luke Blaney <git@lukeblaney.co.uk>
2019-07-21 13:47:16 +0100

    use sqlite for running tests

--- app/settings.py
+++ app/settings.py
@@ -103,16 +103,24 @@ if os.environ.get("PRODUCTION"):
 else:
     DEBUG = True
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'postgres',              # Or path to database file if using sqlite3.
-        'USER': 'postgres',              # Not used with sqlite3.
-        'PASSWORD': '',                  # Not used with sqlite3.
-        'HOST': 'db',                    # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+if os.environ.get("CI"):
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+            'NAME': 'testdb.sqlite3',                    # Set to empty string for default. Not used with sqlite3.
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+            'NAME': 'postgres',              # Or path to database file if using sqlite3.
+            'USER': 'postgres',              # Not used with sqlite3.
+            'PASSWORD': '',                  # Not used with sqlite3.
+            'HOST': 'db',                    # Set to empty string for localhost. Not used with sqlite3.
+            'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+        }
+    }
 
 LOGGING = {
     'version': 1,
