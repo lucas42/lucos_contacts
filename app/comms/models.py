a35175f9c1e9c7701dfe18834726c6a7019c4c56
Luke Blaney <git@lukeblaney.co.uk>
2020-12-29 02:36:09 +0000

    can't edit the year of a Xmas List once created

--- app/comms/models.py
+++ app/comms/models.py
@@ -4,7 +4,7 @@ from agents.models import Agent
 class ChristmasList(models.Model):
 
 	# Calendar year, as per the Gregorian Calendar (Common Era assumed)
-	year = models.IntegerField(primary_key=True)
+	year = models.IntegerField(primary_key=True, editable=False)
 	gave_card_to = models.ManyToManyField(Agent, related_name='gave_card_to')
 	received_card_from = models.ManyToManyField(Agent, related_name='received_card_from')
 
