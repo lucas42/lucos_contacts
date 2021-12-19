d16cd3e26269c681c4603b89e2368117a027e275
Luke Blaney <git@lukeblaney.co.uk>
2021-12-19 00:58:25 +0000

    allow creation of Christmas Card List for years where I haven't yet given/received a card

--- app/comms/models.py
+++ app/comms/models.py
@@ -5,8 +5,8 @@ class ChristmasList(models.Model):
 
 	# Calendar year, as per the Gregorian Calendar (Common Era assumed)
 	year = models.IntegerField(primary_key=True)
-	gave_card_to = models.ManyToManyField(Agent, related_name='gave_card_to')
-	received_card_from = models.ManyToManyField(Agent, related_name='received_card_from')
+	gave_card_to = models.ManyToManyField(Agent, related_name='gave_card_to', blank=True)
+	received_card_from = models.ManyToManyField(Agent, related_name='received_card_from', blank=True)
 
 	def __str__(self):
 		return str(self.year)+" Christmas List"
