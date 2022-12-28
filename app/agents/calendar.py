a3bf85424a28ced18d5b28b2b596ae23b13aaf00
Luke Blaney <git@lukeblaney.co.uk>
2022-12-28 12:55:05 +0000

    improve event labels

--- app/agents/calendar.py
+++ app/agents/calendar.py
@@ -2,15 +2,32 @@ from django.shortcuts import render
 from agents.models import *
 from django.urls import reverse
 from datetime import date
+from django.utils.translation import gettext as _
+from django.contrib.humanize.templatetags.humanize import ordinal
 
 def getBirthdays():
 	agentlist = Agent.objects.filter(starred=True).exclude(day_of_birth__isnull=True).exclude(month_of_birth__isnull=True)
 	birthdays = []
 	for agent in agentlist.distinct():
+		if agent.year_of_birth:
+			currentYear = date.today().year
+			age = currentYear - agent.year_of_birth
+			if nextYear(agent.day_of_birth, agent.month_of_birth):
+				age += 1
+			label = _('%(name)s\'s %(count)s Birthday') % {
+				'name': agent.getName(),
+				'count': ordinal(age)
+			}
+		else:
+			label = _(
+				'%(name)s\'s Birthday'
+			) % {
+				'name': agent.getName()
+			}
 		birthdays.append({
 			'day': agent.day_of_birth,
 			'month': agent.month_of_birth,
-			'label': agent.getName()+"'s Birthday",
+			'label': label,
 			'agent': agent,
 			'link': agent.get_absolute_url(),
 		})
@@ -20,10 +37,11 @@ def getDeathdays():
 	agentlist = Agent.objects.filter(starred=True).exclude(day_of_death__isnull=True).exclude(month_of_death__isnull=True)
 	deathdays = []
 	for agent in agentlist.distinct():
+		label = _('%(name)s\'s Deathday') % {'name': agent.getName()}
 		deathdays.append({
 			'day': agent.day_of_death,
 			'month': agent.month_of_death,
-			'label': agent.getName()+"'s Deathday",
+			'label': label,
 			'agent': agent,
 			'link': agent.get_absolute_url(),
 		})
@@ -31,17 +49,20 @@ def getDeathdays():
 
 def getDates():
 	dates = getBirthdays() + getDeathdays()
-	dates.sort(key=lambda date: (not laterInYear(date['day'], date['month']), date['month'], date['day']))
+	dates.sort(key=lambda date: (nextYear(date['day'], date['month']), date['month'], date['day']))
 	return dates
 
-def laterInYear(day, month):
+# Returns boolean.
+# True if the next instance of this date is next year.
+# False if there's a remaining this year (including today).
+def nextYear(day, month):
 	currentDay = date.today().day
 	currentMonth = date.today().month
 	if month > currentMonth:
-		return True
-	if month < currentMonth:
 		return False
-	return (day >= currentDay)
+	if month < currentMonth:
+		return True
+	return (day < currentDay)
 
 def renderCalendar(request):
 	return render(None, 'agents/index.html', {
