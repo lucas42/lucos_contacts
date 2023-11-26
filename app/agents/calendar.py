202139103da370833f4b0b1a0b420bdbab34dc2a
Luke Blaney <git@lukeblaney.co.uk>
2023-11-26 21:51:08 +0000

    add link to icalendar url from calendar view

--- app/agents/calendar.py
+++ app/agents/calendar.py
@@ -9,6 +9,7 @@ from django.contrib.auth.decorators import login_required
 from django.views.decorators.csrf import csrf_exempt
 from lucosauth.decorators import calendar_auth
 from icalendar import Calendar, Event
+from lucosauth.helpers import get_calendar_key
 
 def getBirthdays():
 	agentlist = Agent.objects.filter(starred=True).exclude(day_of_birth__isnull=True).exclude(month_of_birth__isnull=True)
@@ -114,6 +115,7 @@ def renderCalendar(request):
 		'list': 'calendar',
 		'addurl': reverse('admin:agents_agent_add'),
 		'events': getEvents(),
+		'icalendar_link': '/calendar.ics?key='+get_calendar_key(),
 	})
 
 @csrf_exempt
